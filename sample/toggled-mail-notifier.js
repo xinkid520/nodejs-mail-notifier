a0da754b321e170a44edfdf39f537d5c6c75251f
刘梦博 <liumengbo@liumedeiMac-Pro.local>
2025-02-07 11:06:34 +0800

    修改为自动监听

--- sample/toggled-mail-notifier.js
+++ sample/toggled-mail-notifier.js
@@ -1,39 +1,38 @@
-var notifier = require('../index.js');
+var notifier = require("../index.js");
 
 var started = true;
 
-function toggleState(){
-  if(started){
-    n.stop();
-  }else{
-    n.start();
-  }
-  started = !started;
-  setTimeout(toggleState,3000);
+function toggleState() {
+	if (started) {
+		n.stop();
+	} else {
+		n.start();
+	}
+	started = !started;
+	setTimeout(toggleState, 3000);
 }
 
 var imap = {
-  user: "jerome.creignou",
-  password: "password",
-  host: "imap.host.com",
-  port: 993, // imap port
-  tls: true,// use secure connection
-  tlsOptions: { rejectUnauthorized: false }
+	user: "jerome.creignou",
+	password: "password",
+	host: "imap.host.com",
+	port: 993, // imap port
+	tls: true, // use secure connection
+	tlsOptions: { rejectUnauthorized: false },
 };
 
-var n  = notifier(imap).on('mail', function(mail){
-  console.log(mail);
+var n = notifier(imap).on("mail", function (mail) {
+	console.log(mail);
 });
 
-n.on('end',function(){
-  console.log('...notification ended...');
+n.on("end", function () {
+	console.log("...notification ended...");
 });
 
-n.on('error',function(err){
-  console.log('...notification error : %s', err);
+n.on("error", function (err) {
+	console.log("...notification error : %s", err);
 });
 
 n.start();
 
-setTimeout(toggleState,3000);
-
+setTimeout(toggleState, 3000);
